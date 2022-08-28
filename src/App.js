93208f179dee350139a3b4c72ae62673c06e46ca
xavier <omondix214@gmail.com>
2022-08-28 10:31:02 +0300

    changed api to github dog api

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React,{useEffect, useState} from "react";
 import './App.css';
 const App = () =>{
-  const APP_ID = "441c03af";
-  const APP_KEY = "7d75e44ebc778bec7b42e03dda65ee70";
+  
+  const APP_KEY = "SgXNqcIvgBa7Nor7SvS9m1CPjuiDIQABmXo6A8j8uSniziTafo9sOgVqmkS9OcMk";
 	
   useEffect(() =>{
        getRecipes();
   },[]);
 
   const getRecipes = async() =>{
-    const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`);
+    const response = await fetch(`https://api.thecatapi.com/v1/images/search?limit=10&breed_ids=beng&api_key=${APP_KEY}`);
     const data = await response.json();
     console.log(data);
   }
